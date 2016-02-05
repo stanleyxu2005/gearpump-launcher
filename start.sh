bf332c0b567b5442c1589a3390003bf10deada89
Qian Xu <qian.a.xu@intel.com>
2016-02-05 16:03:12 +0800

    fix bash errors

--- start.sh
+++ start.sh
@@ -12,14 +12,15 @@ if [ -z "$JAVA_OPTS" ]; then
   echo "FATAL: Environment variable 'JAVA_OPTS' is NOT set."
   exit 1
 fi
-export JAVA_OPTS
 
-function set_user_data_opts {
+set_and_export_java_opts() {
   LOG_DIR=/var/log/gearpump
   JARSTORE_DIR=/tmp/gearpump
-  JAVA_OPTS="$JAVA_OPTS -Dgearpump.log.daemon.dir=$LOG_DIR"
-  JAVA_OPTS="$JAVA_OPTS -Dgearpump.log.application.dir=$LOG_DIR"
-  JAVA_OPTS="$JAVA_OPTS -Dgearpump.jarstore.rootpath=$JARSTORE_DIR"
+  JAVA_OPTS="$JAVA_OPTS $@\
+    -Dgearpump.log.daemon.dir=$LOG_DIR \
+    -Dgearpump.log.application.dir=$LOG_DIR \
+    -Dgearpump.jarstore.rootpath=$JARSTORE_DIR"
+  export JAVA_OPTS
 }
 
 COMMAND=$1
@@ -29,23 +30,23 @@ case "$COMMAND" in
   master|local)
     # Launch a container with Gearpump cluster and REST interface (in foreground)
     HOSTNAME=$(hostname)
-    JAVA_OPTS="$JAVA_OPTS -Dgearpump.hostname=$HOSTNAME"
-    JAVA_OPTS="$JAVA_OPTS -Dgearpump.services.host=$HOSTNAME"
-    set_user_data_opts
+    set_and_export_java_opts \
+      "-Dgearpump.hostname=$HOSTNAME" \
+      "-Dgearpump.services.host=$HOSTNAME"
     nohup sh "$SUT_HOME"/bin/services &
     nohup sh "$SUT_HOME"/bin/"$COMMAND" "$@"
     ;;
   worker)
     # Launch a container with a Gearpump worker (in foreground)
-    JAVA_OPTS="$JAVA_OPTS -Dgearpump.hostname=$(hostname -i)"
-    set_user_data_opts
+    set_and_export_java_opts \
+      "-Dgearpump.hostname=$(hostname -i)"
     nohup sh "$SUT_HOME"/bin/worker
     ;;
   gear|storm)
     # Launch a container and execute command `gear` or `storm`
     # Container will be killed, when command is executed. 
-    JAVA_OPTS="$JAVA_OPTS -Dgearpump.hostname=$(hostname -i)"
-    set_user_data_opts
+    set_and_export_java_opts \
+      "-Dgearpump.hostname=$(hostname -i)"
     sh "$SUT_HOME"/bin/"$COMMAND" "$@"
     ;;
   storm-drpc)
